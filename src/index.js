1d445ce82e464b079c6c3ff56742132a40060441
enryu-maker <akifkhan60067@gmail.com>
2024-02-19 15:27:45 +0530

    cc

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import { BrowserRouter } from 'react-router-dom'
