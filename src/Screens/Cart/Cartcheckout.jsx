097337a7e9971960a1ec41a9107f24ec9f06c7c0
enryu-maker <akifkhan60067@gmail.com>
2024-03-21 12:15:49 +0530

    cc

--- src/Screens/Cart/Cartcheckout.jsx
+++ src/Screens/Cart/Cartcheckout.jsx
@@ -22,9 +22,9 @@ function Cartcheckout() {
 
     return (
         <div>
-            <div className="py-14 px-4 md:px-20 2xl:px-40 font-Raleway">
-                <div className="flex justify-between items-center mt-[100px]">
-                    <h1 className="text-3xl  text-gray-800 py-2 font-semibold tracking-widest">Checkout</h1>
+            <div className="py-14 px-16  font-Raleway">
+                <div className="flex justify-center items-center mt-[100px]">
+                    <h1 className="text-3xl  text-gray-800 py-2 tracking-widest">Checkout</h1>
                 </div>
                 <div className="  shadow-lg p-10 flex flex-col grid-cols-3 md:flex-row justify-between space-y-8 md:space-y-0">
                     <div className="w-[60%]">
@@ -32,7 +32,7 @@ function Cartcheckout() {
                             <div key={index} className="flex items-start space-x-4 pb-4 border-b-2">
                                 <img className="w-32 md:w-40 h-auto" src={product?.finish?.images[0]?.image} alt={product.name} />
                                 <div>
-                                    <h2 className="text-xl tracking-widest font-semibold  text-[#df633a]">{product?.name}</h2>
+                                    <h2 className="text-xl tracking-widest font-regular  text-[#df633a]">{product?.name}</h2>
                                     <p className="text-sm tracking-widest text-gray-600">Finish: {product?.finish?.finish?.title}</p>
                                     <p className="text-sm tracking-widest text-gray-600">Size: {product?.price?.size}</p>
                                     <p className="text-sm tracking-widest text-gray-600">Price: ₹{product?.price?.price_map[0]?.price_with_gst}</p>
@@ -46,15 +46,11 @@ function Cartcheckout() {
                     </div>
                     <div className="w-full md:w-1/3 ">
                         <div className="bg-gray-50 shadow-lg p-6">
-                            <h2 className="text-xl font-semibold tracking-wider  text-gray-800">Summary</h2>
+                            <h2 className="text-xl font-regular tracking-wider  text-gray-800">Summary</h2>
                             <div className="flex justify-between items-center mt-4 border-b pb-4">
                                 <p className="text-base text-gray-800">Subtotal</p>
                                 <p className="text-base text-gray-800">₹ {getTotal(cart)}</p>
                             </div>
-                            {/* <div className="flex justify-between items-center mt-4 border-b pb-4">
-                                <p className="text-base text-gray-800">Discount </p>
-                                <p className="text-base text-gray-800">-${summary.discount.amount.toFixed(2)} ({summary.discount.percentage}%)</p>
-                            </div> */}
                             <div className="flex justify-between items-center mt-4 border-b pb-4">
                                 <p className="text-base text-gray-800">Shipping</p>
                                 <p className="text-base text-gray-800">₹ {summary.shipping.toFixed(2)}</p>
