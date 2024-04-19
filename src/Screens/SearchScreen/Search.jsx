53e88d4cf7b818c3ea7cf0b1f558e9a238c09b0f
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-04-20 00:44:18 +0530

    BottomMenu added

---
+++ src/Screens/SearchScreen/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div className=' h-screen text-2xl flex justify-center items-center'>
+      Search
+    </div>
+  );
+}
+
+export default Search;
