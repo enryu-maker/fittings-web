ee7bb4721e839adcc3aa553c5a2abde212088df6
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-04-24 04:05:09 +0530

    footer and search bar updated

--- src/Screens/SearchScreen/Search.jsx
+++ src/Screens/SearchScreen/Search.jsx
@@ -1,61 +1,28 @@
 import React from 'react';
+import { IoSearchSharp } from 'react-icons/io5';
 
 function Search() {
   return (
-    <div className=' h-screen text-2xl flex  justify-center items-center '>
-      <form className='flex items-center max-w-lg mx-auto'>
-        <label
-          htmlFor='voice-search'
-          className='sr-only'>
-          Search
-        </label>
+    <div className=' h-[50vh] text-2xl flex flex-col mt-5  justify-center items-center '>
+      <form className='flex items-center max-w-lg mx-auto w-full p-5'>
         <div className='relative w-full'>
           <input
             type='text'
             id='voice-search'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-deep-orange-300 focus:ring-orange-500 focus:border-orange-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 '
-            placeholder='Search here...'
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md outline-gray-400 focus:ring-orange-500 focus:border-orange-500 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 '
+            placeholder='Search...'
             required=''
           />
           <button
-            type='button'
-            className='absolute inset-y-0 end-0 flex items-center pe-3'>
-            <svg
-              className='w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 16 20'>
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z'
-              />
-            </svg>
+            type='submit'
+            className=' absolute right-2 top-2'>
+            <IoSearchSharp className='text-[#df633a]' />
           </button>
         </div>
-        <button
-          type='submit'
-          className='inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-orange-700 rounded-lg border border-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800'>
-          <svg
-            className='w-4 h-4 me-2'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 20 20'>
-            <path
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
-            />
-          </svg>
-          Search
-        </button>
       </form>
+      <div className='flex'>
+        <h1 className='text-xl text-orange-900'>No Results Found</h1>
+      </div>
     </div>
   );
 }
